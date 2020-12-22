ad374a9bb0a98e620b76d8cf3de9b72a8209e944
Lucas Montoya <lucas.montoya@corbisstudio.com>
2020-12-22 17:16:54 -0300

    se crea vista update

--- turnos/urls.py
+++ turnos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TurnoCreateView, TurnoListView
+from .views import TurnoCreateView, TurnoListView , TurnoUpdateView, TurnoDetailView
 
 app_name = 'turnos'
 
 urlpatterns = [
     path('turno/',TurnoListView.as_view(),name='turno'),
-    path('create_turno/',TurnoCreateView.as_view(),name='create_turno')
+    path('create_turno/',TurnoCreateView.as_view(),name='create_turno'),
+    path('<int:pk>/update_turno',TurnoUpdateView.as_view(),name='update_turno'),
+    path('<int:pk>/<slug:slug>', TurnoDetailView.as_view(), name='detail_turno')
 ]
--- turnos/views.py
+++ turnos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
 from .models import Turno
 from bootstrap_datepicker_plus import DateTimePickerInput
 from .forms import TurnoForm
@@ -13,6 +13,18 @@ class TurnoCreateView(CreateView):
     model = Turno
     form_class = TurnoForm
     success_url = reverse_lazy('turnos:turno')
+
+class TurnoUpdateView(UpdateView):
+    model = Turno
+    form_class = TurnoForm
+    template_name_suffix = "_update_form"
+    # Esto debera volver a otro lado
+    def get_success_url(self):
+        return reverse_lazy('turnos:update_turno', args=[self.object.id]) + '?ok'
+
+
+class TurnoDetailView(DetailView):
+    model = Turno
 
 class TurnoListView(ListView):
     model = Turno
